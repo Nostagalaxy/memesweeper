4fddc7c2222e0b66452bf24b0987d6113c091891
Nostagalaxy <115511353+Nostagalaxy@users.noreply.github.com>
2024-08-17 16:45:37 -0500

    MemeField and Tiles are drawn to screen

---
+++ Engine/MemeField.cpp
@@ -0,0 +1,102 @@
+#include "MemeField.h"
+#include <assert.h>
+#include <random>
+#include "Vei2.h"
+#include "SpriteCodex.h"
+
+MemeField::MemeField(int nMemes)
+{
+	//Check to make sure number of memes parameter is reasonable
+	assert(nMemes > 0 && nMemes < width* height);
+	std::random_device rd;
+	std::mt19937 rng( rd() );
+	std::uniform_int_distribution<int> xDist(0, width - 1);
+	std::uniform_int_distribution<int> yDist(0, height - 1);
+
+	//Spawn number of memes from nMemes
+	for (int nSpawned = 0; nSpawned < nMemes; nSpawned++)
+	{
+		Vei2 spawnPos; 
+		do
+		{
+			spawnPos = { xDist(rng), yDist(rng) };
+		} while (TileAt(spawnPos).HasMeme());
+
+		TileAt(spawnPos).SpawnMeme();
+	}
+}
+
+void MemeField::Draw(Graphics& gfx) const
+{
+	//Draw background rect to color in tiles
+	gfx.DrawRect(GetRect(), SpriteCodex::baseColor);
+
+	//Loop through field and draw each tile
+	for (Vei2 gridPos = { 0,0 }; gridPos.y < height; gridPos.y++)
+	{
+		for (gridPos.x = 0; gridPos.x < width; gridPos.x++)
+		{
+			TileAt(gridPos).Draw( gridPos, gfx );
+		}
+	}
+}
+
+MemeField::Tile& MemeField::TileAt(const Vei2& gridPos)
+{
+	return field[gridPos.y * width + gridPos.x];
+}
+
+const MemeField::Tile& MemeField::TileAt(const Vei2& gridPos) const
+{
+	return field[gridPos.y * width + gridPos.x];
+}
+
+RectI& MemeField::GetRect() const
+{
+	RectI rect = { 0, width * SpriteCodex::tileSize, 0, height * SpriteCodex::tileSize};
+	return rect;
+}
+
+bool MemeField::Tile::HasMeme() const
+{
+	return hasMeme;
+}
+
+void MemeField::Tile::SpawnMeme()
+{
+	assert(!hasMeme);
+	hasMeme = true;
+}
+
+void MemeField::Tile::Draw(const Vei2& gridPos, Graphics& gfx) const
+{
+	Vei2 screenPos = gridPos * SpriteCodex::tileSize;
+
+	switch (state)
+	{
+		case State::Hidden:
+		{
+			SpriteCodex::DrawTileButton(screenPos, gfx);
+			break;
+		}
+		case State::Flagged:
+		{
+			SpriteCodex::DrawTileFlag(screenPos, gfx);
+			break;
+		}
+		case State::Revealed:
+		{
+			//Check if tile has meme
+			if (hasMeme)
+			{
+				SpriteCodex::DrawTile1(screenPos, gfx);
+				break;
+			}
+			else
+			{
+				SpriteCodex::DrawTile0(screenPos, gfx);
+				break;
+			}
+		}
+	}
+}
---
+++ Engine/MemeField.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Graphics.h"
+#include "RectI.h"
+
+class MemeField
+{
+	class Tile
+	{
+	public:
+		enum class State
+		{
+			Hidden,
+			Flagged,
+			Revealed
+		};
+	public:
+		bool HasMeme() const;
+		void SpawnMeme();
+		void Draw(const Vei2& gridPos, Graphics& gfx) const;
+	private:
+		bool hasMeme = false;
+		State state = State::Hidden;
+	};
+public:
+	MemeField(int nMemes);
+	void Draw(Graphics& gfx) const;
+private:
+	Tile& TileAt(const Vei2& gridPos);
+	const Tile& TileAt(const Vei2& gridPos) const;
+private:
+	RectI& GetRect() const;
+	static constexpr int width = 20;
+	static constexpr int height = 16;
+	Tile field[width * height];
+};
